81e0e1ef2fa1bf731a020ffafd07553a9ec1e4f0
hwj <huangweijun1001@gmail.com>
2020-09-09 16:28:53 +0800

    complete HW0 2

---
+++ hw0/2/ClassNameHere.java
@@ -0,0 +1,14 @@
+public class ClassNameHere {
+    /** Returns the maximum value from m. */
+    public static int max(int[] m) {
+        int temp = m[0];
+        for (int i = 1; i < m.length; i++) {
+           if(temp<m[i]) temp = m[i];
+        }
+        return temp;
+    }
+    public static void main(String[] args) {
+       int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6};      
+       System.out.println(max(numbers));
+    }
+}
