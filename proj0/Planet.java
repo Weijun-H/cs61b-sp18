1f9441dafc1c1be4cb3d1095225d8e4eb38fd179
hwj <huangweijun1001@gmail.com>
2020-09-11 16:15:49 +0800

    Pass TestCalcForceExertedBy

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,6 +1,7 @@
 public class Planet{
     public double xxPos, yyPos, xxVel, yyVel, mass;
     public String imgFileName;
+    public static double G = 6.67 * Math.pow(10, -11);
     public Planet(double xP, double yP, double xV, double yV, double m, String img){
         xxPos = xP;
         yyPos = yP;
@@ -17,10 +18,15 @@ public class Planet{
         mass = p.mass;
         imgFileName = p.imgFileName;
     }
+
     public double calcDistance(Planet p){
         double x_2 = Math.pow((xxPos - p.xxPos), 2);
         double y_2 = Math.pow((yyPos - p.yyPos), 2);
         return (Math.pow((x_2 + y_2),0.5));
+    }
+    
+    public double calcForceExertedBy(Planet p){
+        return (G * mass * p.mass / (calcDistance(p) * calcDistance(p)));
     }
     public static void main(String[] args) {
         
