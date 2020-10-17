716c04ea857a5f957a37d4b5e2a08f2dbdfd1b43
hwj <huangweijun1001@gmail.com>
2020-10-17 23:11:42 +0800

    complete Lab5-addHexagon()

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -12,30 +12,60 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
-    public void addHexagon (int side) {
-        int total = side + (side - 1) * 2;
-        for (int i = 0; i < side; i++) {
-            for (int j = side - 1 - i; j > 0 ; j--) {
-                System.out.print(' ');
-            }
-            for (int j = 0; j < side + i * 2; j++) {
-               System.out.print("X");
-            }
-            System.out.println();
+    private static final int WIDTH = 60;
+    private static final int HEIGHT = 60;
+
+    private static class Position{
+        int X,Y;
+        Position(int x, int y){
+            X = x;
+            Y = y;
         }
-        for (int i = side; i > 0; i--) {
-            for (int j = side - i; j > 0 ; j--) {
-                System.out.print(' ');
+    }
+    public void addHexagon (TETile[][] world, Position p, int side, TETile t) {
+        int total = side + (side - 1) * 2;
+        int patternX, patternY;
+        patternX = p.X;
+        patternY = p.Y;
+        for (int i = 0; i < side; i++) {
+            patternX = p.X - i;
+            patternY = p.Y + i;
+            int startX = patternX;
+            for (int j = 0; j < side + i * 2; j++) {
+                world[startX][patternY] = t;
+                startX++;
             }
-            for (int j = 0; j < side + (i - 1) * 2; j++) {
-                System.out.print("X");
+        }
+        for (int i = 0; i < side; i++) {
+            patternY++;
+            int startX = patternX;
+            for (int j = side + (side - 1 - i) * 2; j > 0; j--) {
+                world[startX][patternY] = t;
+                startX++;
             }
-            System.out.println();
+            patternX++;
         }
     }
 
     public static void main(String[] args) {
-        HexWorld HM = new HexWorld();
-        HM.addHexagon(3);
+        // initialize the tile rendering engine with a window of size WIDTH x HEIGHT
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        // initialize tiles
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j <HEIGHT ; j++) {
+                world[i][j] = Tileset.NOTHING;
+            }
+        }
+
+        HexWorld HW = new HexWorld();
+        Position p1 = new Position(15, 15);
+        TETile material = Tileset.WALL;
+        HW.addHexagon(world, p1, 6, material);
+
+        // draws the world to the screen
+        ter.renderFrame(world);
     }
 }
