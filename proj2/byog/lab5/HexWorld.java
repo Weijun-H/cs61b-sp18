2b04befde3b0edf2381cfca955ae0656e9fc8633
hwj <huangweijun1001@gmail.com>
2020-10-17 17:47:01 +0800

    complete Lab5-addHexagon()

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -12,4 +12,30 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    public void addHexagon (int side) {
+        int total = side + (side - 1) * 2;
+        for (int i = 0; i < side; i++) {
+            for (int j = side - 1 - i; j > 0 ; j--) {
+                System.out.print(' ');
+            }
+            for (int j = 0; j < side + i * 2; j++) {
+               System.out.print("X");
+            }
+            System.out.println();
+        }
+        for (int i = side; i > 0; i--) {
+            for (int j = side - i; j > 0 ; j--) {
+                System.out.print(' ');
+            }
+            for (int j = 0; j < side + (i - 1) * 2; j++) {
+                System.out.print("X");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        HexWorld HM = new HexWorld();
+        HM.addHexagon(3);
+    }
 }
