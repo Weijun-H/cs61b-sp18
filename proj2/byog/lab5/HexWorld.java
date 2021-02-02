e1f2346ee8458245b6120e83c28f56f91284e16c
hwj <huangweijun1001@gmail.com>
2021-02-02 18:11:30 +0800

    Recode proj2/Lab5

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -1,6 +1,8 @@
 package byog.lab5;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.util.Random;
+import java.util.Date;
 
 import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
@@ -12,38 +14,97 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
-    private static final int WIDTH = 60;
-    private static final int HEIGHT = 60;
+    private static final int WIDTH = 30;
+    private static final int HEIGHT = 30;
+    private static final int SEED = (int) System.currentTimeMillis();
+    //private static final Random RANDOM = new Random(SEED);
 
     private static class Position{
-        int X,Y;
-        Position(int x, int y){
-            X = x;
-            Y = y;
+        int x,y;
+        Position(int X, int Y){
+            x = X;
+            y = Y;
         }
     }
-    public void addHexagon (TETile[][] world, Position p, int side, TETile t) {
-        int total = side + (side - 1) * 2;
-        int patternX, patternY;
-        patternX = p.X;
-        patternY = p.Y;
-        for (int i = 0; i < side; i++) {
-            patternX = p.X - i;
-            patternY = p.Y + i;
-            int startX = patternX;
-            for (int j = 0; j < side + i * 2; j++) {
-                world[startX][patternY] = t;
-                startX++;
-            }
+
+    /**
+     * Computes the width of row i for a size s hexagon.
+     * @param s The size of the hex.
+     * @param i The row number where i = 0 is the bottom row.
+     * @return
+     */
+    public static int hexRowWidth(int s, int i){
+        int effectiveI = i;
+        if(i >= s){
+            effectiveI = 2 * s  - 1 - effectiveI;
         }
-        for (int i = 0; i < side; i++) {
-            patternY++;
-            int startX = patternX;
-            for (int j = side + (side - 1 - i) * 2; j > 0; j--) {
-                world[startX][patternY] = t;
-                startX++;
-            }
-            patternX++;
+        return s + 2 * effectiveI;
+    }
+
+    /**
+     * Computesrelative x coordinate of the leftmost tile in the ith
+     * row of a hexagon, assuming that the bottom row has an x-coordinate
+     * of zero. For example, if s = 3, and i = 2, this function
+     * returns -2, because the row 2 up from the bottom starts 2 to the left
+     * of the start position, e.g.
+     *   xxxx
+     *  xxxxxx
+     * xxxxxxxx
+     * xxxxxxxx <-- i = 2, starts 2 spots to the left of the bottom of the hex
+     *  xxxxxx
+     *   xxxx
+     *
+     * @param s size of the hexagon
+     * @param i row num of the hexagon, where i = 0 is the bottom
+     * @return
+     */
+    public static int hexRowOffset(int s, int i) {
+        int effectiveI = i;
+        if (i >= s) {
+            effectiveI = 2 * s - 1 - effectiveI;
+        }
+        return -effectiveI;
+    }
+
+    /** Adds a row of the same tile.
+     * @param world the world to draw on
+     * @param p the leftmost position of the row
+     * @param width the number of tiles wide to draw
+     * @param t the tile to draw
+     */
+    public static void addRow(TETile[][] world, Position p, int width, TETile t) {
+        for (int xi = 0; xi < width; xi += 1) {
+            int xCoord = p.x + xi;
+            int yCoord = p.y;
+            world[xCoord][yCoord] = t;
+        }
+    }
+
+    /**
+     * Adds a hexagon to the world.
+     * @param world the world to draw on
+     * @param p the bottom left coordinate of the hexagon
+     * @param s the size of the hexagon
+     * @param t the tile to draw
+     */
+    public static void addHexagon(TETile[][] world, Position p, int s, TETile t) {
+
+        if (s < 2) {
+            throw new IllegalArgumentException("Hexagon must be at least size 2.");
+        }
+
+        // hexagons have 2*s rows. this code iterates up from the bottom row,
+        // which we call row 0.
+        for (int yi = 0; yi < 2 * s; yi += 1) {
+            int thisRowY = p.y + yi;
+
+            int xRowStart = p.x + hexRowOffset(s, yi);
+            Position rowStartP = new Position(xRowStart, thisRowY);
+
+            int rowWidth = hexRowWidth(s, yi);
+
+            addRow(world, rowStartP, rowWidth, t);
+
         }
     }
 
@@ -60,10 +121,13 @@ public class HexWorld {
             }
         }
 
+
         HexWorld HW = new HexWorld();
-        Position p1 = new Position(15, 15);
+        int side = 3;
+        Position p1 = new Position(10, 0);
+        Position p2 = new Position(p1.x + 2 * side - 1, p1.y);
         TETile material = Tileset.WALL;
-        HW.addHexagon(world, p1, 6, material);
+        HW.addHexagon(world, p1, side, material);
 
         // draws the world to the screen
         ter.renderFrame(world);
