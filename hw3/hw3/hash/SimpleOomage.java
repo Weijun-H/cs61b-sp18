aa8d0d3c81335fc4c4bbad6ecb4ce78394f8083a
hwj <huangweijun1001@gmail.com>
2021-04-06 17:09:50 +0800

    finish equals function in SimpleOomage

--- hw3/hw3/hash/SimpleOomage.java
+++ hw3/hw3/hash/SimpleOomage.java
@@ -15,7 +15,11 @@ public class SimpleOomage implements Oomage {
     @Override
     public boolean equals(Object o) {
         // TODO: Write this method.
-        return false;
+        if (o == this) return true;
+        if (o == null) return false;
+        if (o.getClass() != this.getClass()) return false;
+        SimpleOomage that = (SimpleOomage)o;
+        return (this.blue == that.blue) && (this.red == that.red) && (this.green == that.green);
     }
 
     /* Uncomment this method after you've written
