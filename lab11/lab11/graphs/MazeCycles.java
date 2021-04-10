fefd68bf9ad091ffdaf203285dbb7b1a77688cd1
hwj <huangweijun1001@gmail.com>
2021-04-10 10:54:24 +0800

    complete dfs-cycleCheck

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -17,8 +17,25 @@ public class MazeCycles extends MazeExplorer {
     @Override
     public void solve() {
         // TODO: Your code here!
+        dfs(0);
     }
 
     // Helper methods go here
+
+    private void dfs(int v){
+        marked[v] = true;
+        announce();
+        for (int w: maze.adj(v)){
+            if (marked[w]){
+                if (w != edgeTo[v])
+                    return;
+            }else{
+                edgeTo[w] = v;
+                announce();;
+                distTo[w] = distTo[v] + 1;
+                dfs(w);
+            }
+        }
+    }
 }
 
