487e9c52ef6ba982cadfd7b4b70c961cca54070a
hwj <huangweijun1001@gmail.com>
2021-04-10 11:04:22 +0800

    complete A*

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.MinPQ;
+
 /**
  *  @author Josh Hug
  */
@@ -18,9 +20,29 @@ public class MazeAStarPath extends MazeExplorer {
         edgeTo[s] = s;
     }
 
+    private class Node implements Comparable {
+        private int v;
+        private int priority;
+
+        public Node(int v, int dist) {
+            this.v = v;
+            this.priority = dist + h(v);
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            Node n = (Node) o;
+            return this.priority - n.priority;
+        }
+    }
+
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        int vx = maze.toX(v);
+        int vy = maze.toY(v);
+        int tx = maze.toX(t);
+        int ty = maze.toY(t);
+        return Math.abs(vx - tx) + Math.abs(vy - ty);
     }
 
     /** Finds vertex estimated to be closest to target. */
@@ -32,6 +54,26 @@ public class MazeAStarPath extends MazeExplorer {
     /** Performs an A star search from vertex s. */
     private void astar(int s) {
         // TODO
+        MinPQ<Node> pq = new MinPQ<>();
+        Node start = new Node(s, distTo[s]);
+        pq.insert(start);
+        while (!pq.isEmpty()) {
+            Node curr = pq.delMin();
+            marked[curr.v] = true;
+            announce();
+            if (curr.v == t) {
+                return;
+            }
+            for (int adj : maze.adj(curr.v)) {
+                if (distTo[curr.v] + 1 < distTo[adj]) {
+                    distTo[adj] = distTo[curr.v] + 1;
+                    edgeTo[adj] = curr.v;
+                }
+                if (!marked[adj]) {
+                    pq.insert(new Node(adj, distTo[adj]));
+                }
+            }
+        }
     }
 
     @Override
